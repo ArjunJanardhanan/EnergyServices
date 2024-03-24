efca2cb1b587eb79ebf044f2c00c0e5a7cfb1641
ArjunJanardhanan <164714384+ArjunJanardhanan@users.noreply.github.com>
2024-03-24 19:27:50 +0000

    Update dashboard_regression2.py

--- dashboard_regression2.py
+++ dashboard_regression2.py
@@ -27,8 +27,9 @@ feature = df.columns[1:]
 #df_real = pd.read_csv('real_results.csv')
 y2=df['Power[kW]'].values
 
-#Load and run FR model
 
+
+#Load and run LR model
 with open('Lregr.pkl','rb') as file:
     LR_model=pickle.load(file)
 
@@ -41,6 +42,7 @@ MSE_LR=metrics.mean_squared_error(y2,y2_pred_LR)
 RMSE_LR= np.sqrt(metrics.mean_squared_error(y2,y2_pred_LR))
 cvRMSE_LR=RMSE_LR/np.mean(y2)
 NMBE_LR=MBE_LR/np.mean(y2)
+
 #Load RF model
 with open('RF_model.pkl','rb') as file:
     RF_model=pickle.load(file)
@@ -55,16 +57,102 @@ RMSE_RF= np.sqrt(metrics.mean_squared_error(y2,y2_pred_RF))
 cvRMSE_RF=RMSE_RF/np.mean(y2)
 NMBE_RF=MBE_RF/np.mean(y2)
 
+#Load and run SV model
+with open('Sregr.pkl','rb') as file:
+    SV_model=pickle.load(file)
+
+y2_pred_SV = SV_model.predict(X2)
+
+#Evaluate errors
+MAE_SV=metrics.mean_absolute_error(y2,y2_pred_SV) 
+MBE_SV=np.mean(y2-y2_pred_SV)
+MSE_SV=metrics.mean_squared_error(y2,y2_pred_SV)  
+RMSE_SV= np.sqrt(metrics.mean_squared_error(y2,y2_pred_SV))
+cvRMSE_SV=RMSE_SV/np.mean(y2)
+NMBE_SV=MBE_SV/np.mean(y2)
+
+#Load and run DT model
+with open('DT_regr_model.pkl','rb') as file:
+    DT_model=pickle.load(file)
+
+y2_pred_DT = DT_model.predict(X2)
+
+#Evaluate errors
+MAE_DT=metrics.mean_absolute_error(y2,y2_pred_DT) 
+MBE_DT=np.mean(y2-y2_pred_DT)
+MSE_DT=metrics.mean_squared_error(y2,y2_pred_DT)  
+RMSE_DT= np.sqrt(metrics.mean_squared_error(y2,y2_pred_DT))
+cvRMSE_DT=RMSE_DT/np.mean(y2)
+NMBE_DT=MBE_DT/np.mean(y2)
+
+#Load and run GB model
+with open('GB_model.pkl','rb') as file:
+    GB_model=pickle.load(file)
+
+y2_pred_GB = GB_model.predict(X2)
+
+#Evaluate errors
+MAE_GB=metrics.mean_absolute_error(y2,y2_pred_GB) 
+MBE_GB=np.mean(y2-y2_pred_GB)
+MSE_GB=metrics.mean_squared_error(y2,y2_pred_GB)  
+RMSE_GB= np.sqrt(metrics.mean_squared_error(y2,y2_pred_GB))
+cvRMSE_GB=RMSE_GB/np.mean(y2)
+NMBE_GB=MBE_GB/np.mean(y2)
+
+#Load and run XGB model
+with open('XGB_model.pkl','rb') as file:
+    XGB_model=pickle.load(file)
+
+y2_pred_XGB = XGB_model.predict(X2)
+
+#Evaluate errors
+MAE_XGB=metrics.mean_absolute_error(y2,y2_pred_XGB) 
+MBE_XGB=np.mean(y2-y2_pred_XGB)
+MSE_XGB=metrics.mean_squared_error(y2,y2_pred_XGB)  
+RMSE_XGB= np.sqrt(metrics.mean_squared_error(y2,y2_pred_XGB))
+cvRMSE_XGB=RMSE_XGB/np.mean(y2)
+NMBE_XGB=MBE_XGB/np.mean(y2)
+
+#Load and run BT model
+with open('BT_model.pkl','rb') as file:
+    BT_model=pickle.load(file)
+
+y2_pred_BT = BT_model.predict(X2)
+
+#Evaluate errors
+MAE_BT=metrics.mean_absolute_error(y2,y2_pred_BT) 
+MBE_BT=np.mean(y2-y2_pred_BT)
+MSE_BT=metrics.mean_squared_error(y2,y2_pred_BT)  
+RMSE_BT= np.sqrt(metrics.mean_squared_error(y2,y2_pred_BT))
+cvRMSE_BT=RMSE_BT/np.mean(y2)
+NMBE_BT=MBE_BT/np.mean(y2)
+
+
+#Load and run NN model
+with open('NN_model.pkl','rb') as file:
+    NN_model=pickle.load(file)
+
+y2_pred_NN = NN_model.predict(X2)
+
+#Evaluate errors
+MAE_NN=metrics.mean_absolute_error(y2,y2_pred_NN) 
+MBE_NN=np.mean(y2-y2_pred_NN)
+MSE_NN=metrics.mean_squared_error(y2,y2_pred_NN)  
+RMSE_NN= np.sqrt(metrics.mean_squared_error(y2,y2_pred_NN))
+cvRMSE_NN=RMSE_NN/np.mean(y2)
+NMBE_NN=MBE_NN/np.mean(y2)
+
 # Create data frames with predictin results and error metrics 
-d = {'Methods': ['Linear Regression','Random Forest'], 'MAE': [MAE_LR, MAE_RF],'MBE': [MBE_LR, MBE_RF], 'MSE': [MSE_LR, MSE_RF], 'RMSE': [RMSE_LR, RMSE_RF],'cvMSE': [cvRMSE_LR, cvRMSE_RF],'NMBE': [NMBE_LR, NMBE_RF]}
+d = {'Methods': ['Linear Regression','Random Forest Regression','Support Vector Regression','Decision Tree Regression','Gradient Boosting Regression','Extreme Gradient Boosting Regression','Bootstrapping Regression', 'Neural Network Regression'], 'MAE': [MAE_LR, MAE_RF, MAE_SV, MAE_DT, MAE_GB, MAE_XGB, MAE_BT, MAE_NN],'MBE': [MBE_LR, MBE_RF, MBE_SV, MBE_DT, MBE_GB, MBE_XGB, MBE_BT, MBE_NN], 'MSE': [MSE_LR, MSE_RF, MSE_SV, MSE_DT, MSE_GB, MSE_XGB, MSE_BT, MSE_NN], 'RMSE': [RMSE_LR, RMSE_RF, RMSE_SV, RMSE_DT, RMSE_GB, RMSE_XGB, RMSE_BT, RMSE_NN],'cvMSE': [cvRMSE_LR, cvRMSE_RF, cvRMSE_SV, cvRMSE_DT, cvRMSE_GB, cvRMSE_XGB, cvRMSE_BT, cvRMSE_NN],'NMBE': [NMBE_LR, NMBE_RF, NMBE_SV, NMBE_DT, NMBE_GB, NMBE_XGB, NMBE_BT, NMBE_NN]}
 df_metrics = pd.DataFrame(data=d)
-d={'date':df['date'], 'LinearRegression': y2_pred_LR,'RandomForest': y2_pred_RF}
+d={'date':df['date'], 'Linear Regression': y2_pred_LR,'Random Forest Regression': y2_pred_RF,'Support Vector Regression': y2_pred_SV,'Decision Tree Regression': y2_pred_DT,'Gradient Boosting Regression': y2_pred_GB,'Extreme Gradient Boosting Regression': y2_pred_XGB,'Bootstrapping Regression': y2_pred_BT, 'Neural Network Regression': y2_pred_NN}
 df_forecast=pd.DataFrame(data=d)
 
 regression = df_forecast.columns[1:]
 
 # merge real and forecast results and creates a figure with it
 df=pd.merge(df,df_forecast, on='date')
+df=df.dropna()
 # fig2 = px.line(df_forecast,x=df_forecast.columns[0],y=df_forecast.columns[1:])
 
 # Define auxiliary functions
@@ -82,7 +170,6 @@ def generate_table(dataframe, max_rows=10):
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 server=app.server
 app.layout = html.Div([
     html.H1('Energy Forecast Tool', style={'text-align': 'center'}),
@@ -91,6 +178,7 @@ app.layout = html.Div([
         dcc.Tab(label='Raw Data', value='tab-1'),
         dcc.Tab(label='Forecast', value='tab-2'),
         dcc.Tab(label='Error Metrics', value='tab-3'),
+        dcc.Tab(label='Exploratory Data Analysis', value='tab-4')
     ]),
     html.Div(id='tabs-content'),
 ])
@@ -120,7 +208,7 @@ def render_content(tab):
             dcc.Checklist(
                 id='forecast_checklist',
                 options=[{'label': i, 'value': i} for i in regression],
-                value=['RandomForest'],
+                value=['Random Forest Regression'],
                 inline=True
             ),
             html.Br(),
@@ -140,6 +228,24 @@ def render_content(tab):
         return html.Div([
             html.H4('Error metrics for different regression methods', style={'text-align': 'center'}),
                         generate_table(df_metrics)
+        ])
+    elif tab == 'tab-4':
+        return html.Div([
+            html.H4('Exploratory analysis of real data', style={'text-align': 'center'}),
+            html.Label('Select the variable:'),
+            dcc.RadioItems(
+                id='feature-radio',
+                options=[{'label': f, 'value': f} for f in feature],
+                value=feature[0],  # Default value
+                inline=True
+            ),
+            html.Br(),
+            html.Label('Select the plot type:'),
+            dcc.Dropdown(id='plot-type',
+                         options=[{'label': 'Histogram', 'value': 'histogram'},
+                                  {'label': 'Boxplot', 'value': 'boxplot'}],
+                         value='histogram'), 
+            dcc.Graph(id='plot')
         ])
 
 @app.callback(
@@ -173,6 +279,22 @@ def update_graph2(selected_features,selected_feature):
     # Create the figure object
     fig = {'data': traces, 'layout': {'title': 'Selected Features'}}
     return fig
+
+# Callback to update histogram and boxplot based on selected feature
+@app.callback(
+    Output('plot', 'figure'),
+    [Input('feature-radio', 'value'),
+     Input('plot-type', 'value')]
+)
+def update_plots(selected_feature, plot_type):
+    
+    if plot_type == 'histogram':
+        # Create histogram
+        fig3 = px.histogram(df, x=selected_feature, title=f'Histogram for {selected_feature}')
+    else:
+        # Create boxplot
+        fig3 = px.box(df, y=selected_feature, title=f'Boxplot for {selected_feature}')
+    return fig3
 
 
 if __name__ == '__main__':
